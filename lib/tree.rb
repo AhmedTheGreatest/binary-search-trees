c106b0080405324bbc82dbea6d419b8db16f5abb
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-01-07 14:21:52 +0500

    Add build_tree method andconstructor in the Binaryy Tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -4,5 +4,27 @@ require './lib/node'
 
 # A Class representing a Binary Search Tree
 class Tree
+  def initialize(data)
+    @root = build_tree(data)
+  end
 
+  def build_tree(data)
+    if data.empty?
+      return false
+    end
+
+    sorted_data = data.uniq.sort
+    middle = sorted_data.size / 2
+
+    root = Node.new(sorted_data[middle])
+    root.left = build_tree(sorted_data[0...middle])
+    root.right = build_tree(sorted_data[middle+1..])
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
